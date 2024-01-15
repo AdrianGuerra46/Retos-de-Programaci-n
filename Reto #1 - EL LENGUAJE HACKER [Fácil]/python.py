fbfb0c62724d5aa0ecaa5ef37e413995a33931c1
AdrianGuerra <guerra4610@gmail.com>
2024-01-15 14:10:17 -0500

    Reto 1 terminado

---
+++ Reto #1 - EL LENGUAJE HACKER [Fácil]/python.py
@@ -0,0 +1,27 @@
+#diccionario leet
+letters = {
+    "a": "4", "b": "I3", "c": "[", "d": ")", "e": "3", "f": "|=", "g": "&", "h": "#", "i": "1", "j": ",_|", "k": ">|",
+    "l": "1", "m": "/\/\\", "n": "^/", "o": "0", "p": "|*", "q": "(_,)", "r": "|2", "s": "5", "t": "7", "u": "(_)",
+    "v": "\/", "w": "\/\/", "x": "><", "y": "j", "z": "2", "1": "L", "2": "R", "3": "E", "4": "A", "5": "S", "6": "b",
+    "7": "T", "8": "B", "9": "g", "0": "o"
+}
+
+#Definimos la funcion que nos haga las traducciones
+def transhacker (word, diccionario):
+    
+    newword = "" #Creamos la variable para la palabra traducida
+    
+    for l in range(len(word)):
+        letter = word.lower()[l]
+        
+        if letter in diccionario:
+            newword += diccionario[letter]
+        else:
+            newword += letter #Si la letra de la palabra no está en el diccionario, la dejo igual 
+            
+    return newword
+
+userword = input("ingresa la palabra que deseas traducir: ")
+
+translate = transhacker(userword, letters)
+print(translate)
